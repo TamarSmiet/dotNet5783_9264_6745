62c5cf00986c032566c55fd27fee4a2e34a98f06
TamarSmiet <tamarsmiet@gmail.com>
2022-10-01 21:11:22 +0300

    initial code + welcome functions

---
+++ dotNet5783_9264_6745/targil0/Program9264.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Welcome9264();
+            Welcome6745();
+            Console.ReadKey();
+
+        }
+
+        static partial void Welcome6745();
+        private static void Welcome9264()
+        {
+            Console.WriteLine("Enter your name:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first application", userName);
+
+        }
+    }
+}
